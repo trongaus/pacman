e8e375c39ac12e81f5c64998fa116aeafbc66b50
trongaus <trongaus@gmail.com>
2017-04-24 15:51:14 -0400

    working on basic framework

--- src/Player1.py
+++ src/Player1.py
@@ -6,8 +6,39 @@ import sys, pygame
 class Player1(pygame.sprite.Sprite):
 	def __init__(self, gs):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load("../img/pacman-left-closed.png")
-		self.image = pygame.transform.scale2x(self.image)
+		self.image = pygame.transform.scale2x(pygame.image.load("../img/pacman-left-closed.png"))
 		self.rect = self.image.get_rect()
 		self.rect.centerx = gs.rect.centerx
 		self.rect.centery = gs.rect.centery + 128
+		self.speed = 8
+		self.refresh()
+
+	def refresh(self):
+		self.movepos = [0,0]
+
+	def update(self):
+		_new = self.rect.move(self.movepos)
+		self.rect = _new
+		pygame.event.pump()
+
+	def move(self, direction):
+		if direction == 'left':
+			self.movepos[0] = self.movepos[0] - self.speed
+			self.image = pygame.transform.scale2x(pygame.image.load("../img/pacman-left-closed.png"))
+			self.update()
+		elif direction == 'right':
+			self.movepos[0] = self.movepos[0] + self.speed
+			self.image = pygame.transform.scale2x(pygame.image.load("../img/pacman-right-closed.png"))
+			self.update()
+		elif direction == 'up':
+			self.movepos[1] = self.movepos[1] - self.speed
+			self.image = pygame.transform.scale2x(pygame.image.load("../img/pacman-up-closed.png"))
+			self.update()
+		elif direction == 'down':
+			self.movepos[1] = self.movepos[1] + self.speed
+			self.image = pygame.transform.scale2x(pygame.image.load("../img/pacman-down-closed.png"))
+			self.update()
+		self.refresh()
+		
+		
+		
--- src/main.py
+++ src/main.py
@@ -12,9 +12,13 @@ class GameSpace:
 		self.size = self.width, self.height = 640, 496
 		self.black = 0,0,0
 		self.screen = pygame.display.set_mode(self.size)
+		pygame.display.set_caption('Pac-Man')
+		self.logo = pygame.image.load("../img/logo.png")
+		self.logorect = self.logo.get_rect()
+		self.logorect.centerx = 546
+		self.logorect.centery = 28
 		self.image = pygame.image.load("../img/full-board.png")
 		self.image = pygame.transform.scale2x(self.image)
-		pygame.display.set_caption('Pac-Man')
 		self.rect = self.image.get_rect()
 		self.rect.centerx = 224 
 		self.rect.centery = 248
@@ -28,7 +32,30 @@ class GameSpace:
 		self.pink_ghost.rect.centerx -= 30
 		self.orange_ghost = gh.Ghost(self, "ghost-orange-up.png")
 
+	def update(self):
+		self.screen.fill(self.black)
+		self.screen.blit(self.image, self.rect)
+		self.screen.blit(self.logo, self.logorect)
+		self.screen.blit(self.player1.image, self.player1.rect)
+		self.screen.blit(self.red_ghost.image, self.red_ghost.rect)
+		self.screen.blit(self.blue_ghost.image, self.blue_ghost.rect)
+		self.screen.blit(self.pink_ghost.image, self.pink_ghost.rect)
+		self.screen.blit(self.orange_ghost.image, self.orange_ghost.rect)
+
+	def start(self):
+		clicked = False
+		self.update()
+		pygame.draw.rect(self.screen, (255,255,255), (480,200,60,30))
+		pygame.display.update()			
+		while not clicked:
+			for event in pygame.event.get():
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					pos = pygame.mouse.get_pos()
+					print(pos)
+		self.main()
+
 	def main(self):
+		pygame.key.set_repeat(1,100)
 		while 1:
 			# clock tick
 			self.clock.tick(60)
@@ -38,25 +65,19 @@ class GameSpace:
 					sys.exit()
 				if event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_LEFT:
-						print("K_LEFT")
+						self.player1.move('left')
 					elif event.key == pygame.K_RIGHT:
-						print("K_RIGHT")
+						self.player1.move('right')
 					elif event.key == pygame.K_UP:
-						print("K_UP")
+						self.player1.move('up')
 					elif event.key == pygame.K_DOWN:
-						print("K_DOWN")
+						self.player1.move('down')
 
 			# update the screen
-			self.screen.fill(self.black)
-			self.screen.blit(self.image, self.rect)
-			self.screen.blit(self.player1.image, self.player1.rect)
-			self.screen.blit(self.red_ghost.image, self.red_ghost.rect)
-			self.screen.blit(self.blue_ghost.image, self.blue_ghost.rect)
-			self.screen.blit(self.pink_ghost.image, self.pink_ghost.rect)
-			self.screen.blit(self.orange_ghost.image, self.orange_ghost.rect)
+			self.update()
 			pygame.display.flip()
 
 # run main
 if __name__ == '__main__':
 	gs = GameSpace()
-	gs.main()
+	gs.start()
