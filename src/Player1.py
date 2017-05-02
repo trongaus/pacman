9e4c84bf99f4585b8d4b7a631f6553b86ef2c5a4
Taylor Rongaus <trongaus@taylors-mbp.dhcp.nd.edu>
2017-05-02 18:29:23 -0400

    collision detection works wooooo

--- src/Player1.py
+++ src/Player1.py
@@ -13,39 +13,43 @@ class Player1(pygame.sprite.Sprite):
 		self.rect.centerx = gs.rect.centerx
 		self.rect.centery = gs.rect.centery + 128
 		self.speed = 8
-		self.refresh()
+		self.refresh(gs)
 
-	def refresh(self):
+	def refresh(self, gs):
 		self.movepos = [0,0]
 
-	def update(self):
+	def update(self, gs):
 		_new = self.rect.move(self.movepos)
-		self.rect = _new
-		pygame.event.pump()
+		if gs.board[int(_new.centery/self.speed)][int(_new.centerx/self.speed)] == '1':
+			self.rect = _new
+			pygame.event.pump()
 
 	# move the pacman piece and update the image depending on the direction
 	def move(self, gs, direction):
+		# start by updating the image regardless of if there's a barrier
 		if direction == 'left':
-			self.movepos[0] = self.movepos[0] - self.speed
 			self.image = pygame.transform.scale2x(pygame.image.load("../img/pacman-left-closed.png"))
-			self.update()
-			#print(self.rect.centerx/self.speed, self.rect.centery/self.speed)
 		elif direction == 'right':
-			self.movepos[0] = self.movepos[0] + self.speed
 			self.image = pygame.transform.scale2x(pygame.image.load("../img/pacman-right-closed.png"))
-			self.update()
-			#print(self.rect.centerx/self.speed, self.rect.centery/self.speed)
 		elif direction == 'up':
-			self.movepos[1] = self.movepos[1] - self.speed
 			self.image = pygame.transform.scale2x(pygame.image.load("../img/pacman-up-closed.png"))
-			self.update()
-			#print(self.rect.centerx/self.speed, self.rect.centery/self.speed)
 		elif direction == 'down':
-			self.movepos[1] = self.movepos[1] + self.speed
 			self.image = pygame.transform.scale2x(pygame.image.load("../img/pacman-down-closed.png"))
-			self.update()
-			#print(self.rect.centerx/self.speed, self.rect.centery/self.speed)
-		self.refresh()
+		# then check the board of 0s and 1s - if its a 1, there's a path so you can take it
+		try: 
+			if gs.board[int(self.rect.centery/self.speed)][int(self.rect.centerx/self.speed)] == '1':
+				if direction == 'left':
+					self.movepos[0] = self.movepos[0] - self.speed
+				elif direction == 'right':
+					self.movepos[0] = self.movepos[0] + self.speed
+				elif direction == 'up':
+					self.movepos[1] = self.movepos[1] - self.speed
+				elif direction == 'down':
+					self.movepos[1] = self.movepos[1] + self.speed
+				self.update(gs)
+				self.refresh(gs)
+		except:
+			pass
 		
 		
 		
